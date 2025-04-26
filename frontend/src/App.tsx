0041553fca580fd23e8aeaf128a41b22f858bc1f
Tareq Radi <raditareq15@gmail.com>
2025-04-26 14:43:49 +0300

    v1- installation react with typescript and tailwindcss with shadcn

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+const App = () => {
+  return (
+    <div className="flex flex-col items-center  justify-center min-h-svh">
+    <Button >Click me</Button>
+  </div>
+
+  )
+}
+
+export default App
