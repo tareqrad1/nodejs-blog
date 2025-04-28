ab243a7f584342dd03aca607e042d7b2d72a5fbd
Tareq Radi <raditareq15@gmail.com>
2025-04-28 12:39:19 +0300

    delete oldest components & start new

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,8 @@
-import { Route, Routes } from "react-router-dom"
-import HomePage from "./pages/home/HomePage"
-import SignupPage from "./pages/auth/SignupPage"
-import ArticlePage from "./pages/home/ArticlePage"
-import LoginPage from "./pages/auth/LoginPage"
+import React from 'react'
 
 const App = () => {
   return (
-    <>
-      <Routes>
-        <Route path="*" element={<h1 className="text-3xl text-center h-screen">Not Found!</h1>} />
-        <Route path="/" element={<HomePage />}/>
-        <Route path="/signup" element={<SignupPage />}/>
-        <Route path="/article" element={<ArticlePage />}/>
-        <Route path="/login" element={<LoginPage />}/>
-      </Routes>
-    </>
+    <div>hello word!</div>
   )
 }
 
--- frontend/src/pages/home/ArticlePage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ArticlePage: React.FC = (): React.JSX.Element => {
-  return (
-    <div>Article</div>
-  )
-}
-
-export default ArticlePage
